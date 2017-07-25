bf597d1fbfe9e5ca0a19717fe314d43dc19eea25
Md. Razon Hossain <deadmantheimmortal@gmail.com>
2017-07-26 01:11:41 +0600

    FirstCommit

---
+++ app/src/main/java/razon/nctballbooksfree/Model/Book.java
@@ -0,0 +1,72 @@
+package razon.nctballbooksfree.Model;
+
+/**
+ * Created by razon30 on 26-07-17.
+ */
+
+public class Book {
+
+    String name;
+    String url;
+    String writter;
+    String size;
+    String cls;
+    String id;
+
+    public Book(String name, String url, String writter, String size, String cls, String id) {
+        this.name = name;
+        this.url = url;
+        this.writter = writter;
+        this.size = size;
+        this.cls = cls;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getWritter() {
+        return writter;
+    }
+
+    public void setWritter(String writter) {
+        this.writter = writter;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCls() {
+        return cls;
+    }
+
+    public void setCls(String cls) {
+        this.cls = cls;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/razon/nctballbooksfree/Model/vs.java
@@ -0,0 +1,8 @@
+package razon.nctballbooksfree.Model;
+
+/**
+ * Created by razon30 on 26-07-17.
+ */
+
+public class vs {
+}
